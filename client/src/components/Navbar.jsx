2d44df9d3afdd74c7a942bbf293f51a7ea618c78
Ian Daluz <janiandaluz@yahoo.com>
2024-09-26 08:01:39 -0600

    fixing styling for logout button

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -101,7 +101,7 @@ const Navbar = () => {
           <li>
             <Link to="/products"
               className={`text-lg hover:bg-gray-700 p-4 rounded ${
-                activeSection === 'booking' ? 'text-cyan-600' : ''
+                activeSection === 'products' ? 'text-cyan-600' : ''
               }`}
               onClick={handleLinkClick}
             >
@@ -123,12 +123,13 @@ const Navbar = () => {
                 </Link>
               </li>
               <li>
-                <button
+                <Link
+                  to="/"
                   onClick={logout}
                   className="text-lg hover:bg-gray-700 p-4 rounded"
                 >
                   Logout
-                </button>
+                </Link>
               </li>
             </>
           ) : (
@@ -190,9 +191,9 @@ const Navbar = () => {
                   </Link>
                 </li>
                 <li>
-                  <button onClick={() => { handleLinkClick(); logout(); }} className="text-gray-800 text-lg hover:bg-gray-200 p-2 rounded">
+                  <Link to="/" onClick={() => { handleLinkClick(); logout(); }} className="text-gray-800 text-lg hover:bg-gray-200 p-2 rounded">
                     Logout
-                  </button>
+                  </Link>
                 </li>
               </>
             ) : (
